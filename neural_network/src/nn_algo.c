dbbd171e8d5e8dc013a94a23d016cb00acd3593d
jar3m <jar3m4@gmail.com>
2021-01-21 01:33:59 +0530

    Changed nn algo to support regression backprop and feedforward Iris example working BC Diag example working

--- neural_network/src/nn_algo.c
+++ neural_network/src/nn_algo.c
@@ -1,90 +1,66 @@
 #include "nn_def.h" 
 
-void feed_forward_layer(t_layer *l) 
+void feed_forward_layer(t_layer *l, e_nntype type) 
 {
 	int i,j;
 	float linear_out;
 
-	//TODO parallelize	
-	for(i = 0; i < l->n_output; i++) {
+	for (i = 0; i < l->n_output; i++) {
 		linear_out = 0;
-		if(l->neuron[i].by_pass) {
-			linear_out = l->input[(int)l->neuron[i].weight[0]];
+		for (j = 0; j < l->n_input; j++) {
+			linear_out += l->input[j] * l->neuron[i].weight[j];
 		}
-		else {
-			for(j = 0; j < l->n_input; j++) {
-				linear_out += l->input[j] * l->neuron[i].weight[j];
-			}
-		}
-#if NN_TYPE == NN_REGRESS
-		if(l->layer_type == eOUTPUT) 	
+		if (l->layer_type == eOUTPUT && type == eREGRESS) {
 			l->output[i] = linear_out;
-		else{
-#endif
-		if(!l->neuron[i].by_pass) {
+		} else {
 			l->output[i] = l->neuron[i].activate(linear_out);
 		}
-		else {
-			l->output[i] = linear_out;
-		}
-#if NN_TYPE == NN_REGRESS
-		}
-#endif
 	}
 }
 
-void back_propogate_layer(t_layer *l, float eta) 
+void back_propogate_layer(t_layer *l, float eta, e_nntype type) 
 {
 	int i, j;
 	float err_op;
 
-	if(l->layer_type == eOUTPUT) {	
-	//TODO parallelize	
-		for(i = 0; i < l->n_output; i++) {
-#if NN_TYPE == NN_REGRESS
-			l->o_back_err[i] = l->error[i];
-#else
-			l->o_back_err[i] = l->error[i] * l->neuron[i].deactivate(l->output[i]);
-#endif
+	if (l->layer_type == eOUTPUT) {	
+		for (i = 0; i < l->n_output; i++) {
+			if (l->layer_type == eOUTPUT && type == eREGRESS) {
+				l->o_back_err[i] = l->error[i];
+			} else {
+				l->o_back_err[i] = l->error[i] * l->neuron[i].deactivate(l->output[i]);
+			}
 		}
 	}
 
-	//TODO parallelize	
 	/* calculate error for the pevious layer */
-	for(i = 0; i < l->n_input; i++) {
-		if(!l->neuron[i].by_pass) {
-			for(err_op = j = 0; j < l->n_output; j++) {
-				if(!l->neuron[j].by_pass) 
-					err_op += (l->neuron[j].weight[i] * l->o_back_err[j]);
-			}
-			l->i_back_err[i] = err_op * l->neuron[0].deactivate(l->input[i]);
+	for (i = 0; i < l->n_input; i++) {
+		for (err_op = j = 0; j < l->n_output; j++) {
+			err_op += (l->neuron[j].weight[i] * l->o_back_err[j]);
 		}
-		else
-			l->i_back_err[i] = 0;
+		l->i_back_err[i] = err_op * l->neuron[0].deactivate(l->input[i]);
 	}
 
-	//TODO parallelize	
-	/* update weights */
-	for(i = 0; i < l->n_output; i++) {
-		if(!l->neuron[i].by_pass) 
-			for(j = 0; j < l->n_input; j++) {
-				l->neuron[i].weight[j] = l->neuron[i].weight[j] + (eta * l->o_back_err[i] * l->input[j]);
-			}
+	for (i = 0; i < l->n_output; i++) {
+		for(j = 0; j < l->n_input; j++) {
+			l->neuron[i].weight[j] = l->neuron[i].weight[j] + (eta * l->o_back_err[i] * l->input[j]);
+		}
 	}
-
 }
 
 void feed_forward(t_neural_network *nwk, float *input)
 {
 	int i;
 	
-	//TODO GET INPUT
-	for(i = 0; i < nwk->i_layer->n_input; i++)
+	for(i = 0; i < nwk->i_layer->n_input; i++) {
 		nwk->i_layer->input[i] = input[i];
-	for(i = 0; i < nwk->n_hlayer; i++)
-		feed_forward_layer(nwk->h_layer[i]);
+	}
+
+	for(i = 0; i < nwk->n_hlayer; i++) {
+		feed_forward_layer(nwk->h_layer[i], nwk->type);
+	}
 	
-	feed_forward_layer(nwk->o_layer);
+	feed_forward_layer(nwk->o_layer, nwk->type);
 
 }
 
@@ -98,13 +74,13 @@ void back_propogate(t_neural_network *nwk, float *output)
 	}
 
 //	print_layer(nwk->o_layer);
-	back_propogate_layer(nwk->o_layer, nwk->eta);
+	back_propogate_layer(nwk->o_layer, nwk->eta, nwk->type);
 //	print_layer(nwk->o_layer);
 	i = nwk->n_hlayer;
 	do {
 		--i;
 //		print_layer(nwk->h_layer[i]);
-		back_propogate_layer(nwk->h_layer[i], nwk->eta);
+		back_propogate_layer(nwk->h_layer[i], nwk->eta, nwk->type);
 //		print_layer(nwk->h_layer[i]);
 	}while (i);
 }
